6cea8d98f58522a6277e7250a1d5a7127a11a613
Paulo Eduardo Cunha <cunhapauloeduardo10@gmail.com>
2025-06-22 00:19:50 -0300

    Reorganiza projeto: move 'mysite' para 'config' e 'api' para 'apps'

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for mysite project.
+URL configuration for config project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
@@ -33,5 +33,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),  # 👈 isso conecta suas rotas
+    path('api/', include('apps.api.urls')),  # 👈 isso conecta suas rotas
 ]
